e575905dfb3df62b11353565abcbff7ecc0c3681
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2021-08-24 17:35:57 +0530

    Revert "Merge branch 'main' of https://github.com/Prashoon123/olympics-activity into main"
    This reverts commit aa16a97808d4e03cbe9c41e3224bce974f9fef1a, reversing
    changes made to afaedfe21b6ce0216ea3bc041d9d86240c498dcf.

--- src/components/DisclosureComponent.js
+++ src/components/DisclosureComponent.js
@@ -1,21 +1,21 @@
 import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/solid";
 
-function DisclosureComponent({ question, answer, className, qClassName }) {
+function DisclosureComponent({ question, answer, className }) {
   return (
     <div className={className}>
       <Disclosure>
         {({ open }) => (
           <>
-            <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-500 bg-gray-900 rounded-lg hover:bg-black focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75 items-center">
-              <span className={qClassName}>{question}</span>
+            <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-white rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+              <span>{question}</span>
               <ChevronUpIcon
                 className={`${
                   open ? "" : "transform rotate-180"
                 } w-8 h-8 text-purple-500`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-purple-500 bg-gray-900 hover:bg-black rounded-lg">
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-purple-900 bg-white rounded-lg">
               {answer}
             </Disclosure.Panel>
           </>
